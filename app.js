60f029fdf18b6d973591caa3cb9444e9647cebfc
Austin Pearce <austinpearce1005@gmail.com>
2017-07-01 23:26:47 -0400

    Initial commit, 1.0.0

---
+++ app.js
@@ -0,0 +1,86 @@
+/*jshint esversion: 6 */
+
+/*******BEGIN CONFIG*******/
+const amountToBuy = 1500; //Amount to buy, IN CENTS, NOT DOLLARS;
+const apiKey = '';
+const apiSecret = '';
+const passphrase = '';
+
+//When to buy
+const hour = 16; //Remember to use 24 hour time
+const minute = 20;
+const second = 0;
+/********END CONFIG********/
+
+const assert = require('assert');
+const Gdax = require('gdax');
+let client = new Gdax.AuthenticatedClient(apiKey, apiSecret, passphrase);
+let buyTimes = [];
+
+//Set trading pair to ETH-USD
+client.productID = 'ETH-USD';
+
+function refreshPrice(callback) {
+    client.getProductTicker((err, res, data) => {
+        if(!err) {
+            callback(null, data.ask);
+        } else {
+            callback(err);
+        }
+    });
+}
+
+function placeOrder(size) {
+    let buyParams = {
+        'type': 'market',
+        'size': size,
+        'side': 'buy',
+        'product_id': 'ETH-USD'
+    };
+    console.log('Placing market order');
+    client.buy(buyParams, function(err, res, data) {
+        if(!err) {
+            console.log('Buy sucessfully placed.');
+            console.dir(data);
+            queueOrder();
+        } else {
+            console.log('Error placing buy. Retrying in 60s.');
+            return setTimeout(buyEth, 60 * 1000);
+        }
+    });
+}
+
+function buyEth() {
+    refreshPrice(function(err, price) {
+        if(!err) {
+            console.log('-----------------------------------');
+            console.log(`Latest ask price is $${parseFloat(price).toFixed(2)} USD/ETH`);
+            price = Math.floor(parseFloat(price) * 100); //Converts price into an integer in cents
+            assert(Number.isInteger(price), 'Assert failed: variable price did not hold an integer value');
+            let size = (amountToBuy / price).toFixed(8);
+            console.log(`Buying $${amountToBuy/100} worth of ETH (${size} ETH)`);
+            placeOrder(size);
+        } else {
+            console.log('Error getting price. Retrying in 60s.');
+            return setTimeout(buyEth, 60 * 1000);
+        }
+    });
+}
+
+function queueOrder() {
+    let now = new Date();
+    let timeToBuy = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute, second);
+    if(timeToBuy.getTime() < now.getTime()) {
+        timeToBuy = new Date(timeToBuy.getTime() + 1000 * 60 * 60 * 24);
+    }
+    if(buyTimes.indexOf(timeToBuy.getTime()) >= 0) {
+        return;
+    } else {
+        console.log(`Placing next buy order @ ${timeToBuy.toLocaleString()}`);
+        setTimeout(buyEth, timeToBuy.getTime() - now.getTime());
+        buyTimes.push(timeToBuy.getTime());
+    }
+}
+
+//Start the timer
+queueOrder();
